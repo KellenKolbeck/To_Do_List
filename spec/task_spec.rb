3145223ce060a1cbb1b2a42259a403df66446943
travis crampton & kellen kolbeck <student@epicodus.com>
2015-08-17 11:50:43 -0700

    Add spec and code to return all task descriptions

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -11,5 +11,37 @@ RSpec.configure do |config|
 end
 
 describe(Task) do
+  describe("#==") do
+    it("is the same task if it has the same description") do
+      task1 = Task.new({:description => "learn SQL"})
+      task2 = Task.new({:description => "learn SQL"})
+      expect(task1).to(eq(task2))
+    end
+  end
 
+  describe('.all') do
+    it("starts as an empty array for tasks") do
+      expect(Task.all()).to(eq([]))
+    end
+  end
+  describe('#save') do
+    it("saves each task added to the array") do
+      test_description = Task.new({:description => "feed the chicken"})
+      test_description.save()
+      expect(Task.all()).to(eq([test_description]))
+    end
+  end
+  describe('.clear') do
+    it("clears out tasks from the array") do
+      Task.new("pet the lion").save()
+      Task.clear()
+      expect(Task.all()).to(eq([]))
+    end
+  end
+  describe("#description") do
+    it("gives the description of the task") do
+      test_description = Task.new("scrub the zebra")
+      expect(test_description.description()).to(eq("scrub the zebra"))
+    end
+  end
 end
--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -1,34 +1,34 @@
-require('rspec')
-require('to_do')
-
-describe(Task) do
-  before() do
-    Task.clear()
-  end
-
-  describe("#description") do
-    it("gives the description of the task") do
-      test_description = Task.new("scrub the zebra")
-      expect(test_description.description()).to(eq("scrub the zebra"))
-    end
-  end
-  describe('.all') do
-    it("starts as an empty array for tasks") do
-      expect(Task.all()).to(eq([]))
-    end
-  end
-  describe('#save') do
-    it("saves each task added to the array") do
-      test_description = Task.new("feed the chicken")
-      test_description.save()
-      expect(Task.all()).to(eq([test_description]))
-    end
-  end
-  describe('.clear') do
-    it("clears out tasks from the array") do
-      Task.new("pet the lion").save()
-      Task.clear()
-      expect(Task.all()).to(eq([]))
-    end
-  end
-end
+# require('rspec')
+# require('task')
+#
+# describe(Task) do
+#   before() do
+#     Task.clear()
+#   end
+#
+#   describe("#description") do
+#     it("gives the description of the task") do
+#       test_description = Task.new("scrub the zebra")
+#       expect(test_description.description()).to(eq("scrub the zebra"))
+#     end
+#   end
+#   describe('.all') do
+#     it("starts as an empty array for tasks") do
+#       expect(Task.all()).to(eq([]))
+#     end
+#   end
+#   describe('#save') do
+#     it("saves each task added to the array") do
+#       test_description = Task.new("feed the chicken")
+#       test_description.save()
+#       expect(Task.all()).to(eq([test_description]))
+#     end
+#   end
+#   describe('.clear') do
+#     it("clears out tasks from the array") do
+#       Task.new("pet the lion").save()
+#       Task.clear()
+#       expect(Task.all()).to(eq([]))
+#     end
+#   end
+# end
