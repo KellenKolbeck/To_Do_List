3d96fe2c53eae7162e47367ce7c6c8d3943df5bb
travis crampton & kellen kolbeck <student@epicodus.com>
2015-08-17 13:49:21 -0700

    update the task_spec

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -13,8 +13,8 @@ end
 describe(Task) do
   describe("#==") do
     it("is the same task if it has the same description") do
-      task1 = Task.new({:description => "learn SQL"})
-      task2 = Task.new({:description => "learn SQL"})
+      task1 = Task.new({:description => "learn SQL", :list_id => 1})
+      task2 = Task.new({:description => "learn SQL", :list_id => 1})
       expect(task1).to(eq(task2))
     end
   end
@@ -24,23 +24,25 @@ describe(Task) do
       expect(Task.all()).to(eq([]))
     end
   end
-  # describe('#save') do
-  #   it("saves each task added to the array") do
-  #     test_description = Task.new({:description => "feed the chicken"})
-  #     test_description.save()
-  #     expect(Task.all()).to(eq([test_description]))
-  #   end
-  # end
-  # describe('.clear') do
-  #   it("clears out tasks from the array") do
-  #     Task.new("pet the lion").save()
-  #     Task.clear()
-  #     expect(Task.all()).to(eq([]))
-  #   end
-  # end
+
+  describe("#save") do
+    it("adds a task to the array of saved tasks") do
+      test_task = Task.new({:description => "learn sql", :list_id => 1})
+      test_task.save()
+      expect(Task.all()).to(eq([test_task]))
+    end
+  end
+
+  describe("#list_id") do
+    it("lets you read the list ID out") do
+      test_task = Task.new({:description => "learn sql", :list_id => 1})
+      expect(test_task.list_id()).to(eq(1))
+    end
+  end
+
   describe("#description") do
     it("gives the description of the task") do
-      test_description = Task.new({:description => "scrub the zebra"})
+      test_description = Task.new({:description => "scrub the zebra", :list_id => 1})
       expect(test_description.description()).to(eq("scrub the zebra"))
     end
   end
